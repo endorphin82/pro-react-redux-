e59698abea75ac2d62289fc80cf38e02551389b1
endorphin82 <endorphin82@gmail.com>
2019-02-04 14:15:01 +0200

    0028-project_structure

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello Word</h1>, document.getElementById('root'));
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
+
+const App = () => {
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
