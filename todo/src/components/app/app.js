29ec285977378bbc68ef906d6662e755eddda22e
endorphin82 <endorphin82@gmail.com>
2019-02-06 08:47:07 +0200

    0040-set_state_delete_element

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
-import TodoList from '../todo-list';
-import ItemStatusFilter from '../item-status-filter';
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
 
-import './app.css';
+import "./app.css";
 
-const App = () => {
+export default class App extends Component {
+  constructor() {
+    super();
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+    this.state = {
+      todoData: [
+        { label: "Drink Coffee", important: false, id: 1 },
+        { label: "Make Awesome App", important: true, id: 2 },
+        { label: "Have a lunch", important: false, id: 3 }
+      ]
+    };
+  }
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  //bind(this)
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      // [a, b, c, d, e]
+      // [a, b,  , d, e]
+      const newArray = [
+    ...todoData.slice(0, idx),
+    ...todoData.slice(idx + 1)
+    ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render() {
+    const { todoData } = this.state;
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel/>
+          <ItemStatusFilter/>
+        </div>
+
+        <TodoList
+          todos={todoData}
+          onDeleted={this.deleteItem}
+        />
       </div>
-
-      <TodoList
-        todos={todoData}
-        onDeleted={ id => console.log('Deleted', id)}
-      />
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
