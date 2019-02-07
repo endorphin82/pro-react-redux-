390a4d54222da928d834d39c3456f4749324231d
endorphin82 <endorphin82@gmail.com>
2019-02-07 18:12:29 +0200

    0048-filters

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -21,7 +21,8 @@ export default class App extends Component {
         this.createTodoItem("Make Awesome App"),
         this.createTodoItem("Have a lunch")
       ],
-      term: ""
+      term: "",
+      filter: "all" // active, all, done
     };
   }
 
@@ -84,8 +85,16 @@ export default class App extends Component {
     );
   };
 
-  search(items, term) {
 
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
+
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
+  search(items, term) {
     if (term.length === 0) {
       return items;
     }
@@ -96,15 +105,24 @@ export default class App extends Component {
     });
   }
 
-  onSearchChange = (term) => {
-    this.setState({ term });
-  };
+  filter(items, filter) {
+    switch (filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter(item => !item.done);
+      case "done":
+        return items.filter(item => item.done);
+      default:
+        return items;
+    }
+  }
 
   render() {
 
-    const { todoData, term } = this.state;
+    const { todoData, term, filter } = this.state;
 
-    const visibleItems = this.search(todoData, term);
+    const visibleItems = this.filter(this.search(todoData, term), filter);
 
     const doneCount = todoData
       .filter((el) => el.done === true)
@@ -117,7 +135,10 @@ export default class App extends Component {
         <AppHeader toDo={todoCount} done={doneCount}/>
         <div className="top-panel d-flex">
           <SearchPanel onSearchChange={this.onSearchChange}/>
-          <ItemStatusFilter/>
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange}
+          />
         </div>
 
         <TodoList
--- todo/src/components/item-status-filter/item-status-filter.js
+++ todo/src/components/item-status-filter/item-status-filter.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 
-import './item-status-filter.css'
+import "./item-status-filter.css";
 
 export default class ItemStatusFilter extends Component {
+  buttons = [
+    { name: "all", label: "All" },
+    { name: "active", label: "Active" },
+    { name: "done", label: "Done" }
+  ];
 
   render() {
+    const { filter, onFilterChange } = this.props;
+
+    const buttons = this.buttons.map(({ name, label }) => {
+      return (
+        <button
+          onClick={() => onFilterChange(name)}
+          key={name}
+          type="button"
+          className={`btn ${filter === name
+            ? "btn-info"
+            : "btn-outline-secondary"}`}>{label}
+        </button>
+      );
+    });
+
     return (
       <div className="btn-group">
-        <button type="button"
-                className="btn btn-info">All
-        </button>
-        <button type="button"
-                className="btn btn-outline-secondary">Active
-        </button>
-        <button type="button"
-                className="btn btn-outline-secondary">Done
-        </button>
+        {buttons}
       </div>
     );
   }
