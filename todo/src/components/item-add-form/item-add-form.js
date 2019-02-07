3825f9f99e595e4301a782315858cd9eaa1a538e
endorphin82 <endorphin82@gmail.com>
2019-02-07 15:27:36 +0200

    0044-handling_forms

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -3,22 +3,37 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+
+  state = {
+    label: ""
+  };
+
+  onLabelChange = (e) => {
+    this.setState({ label: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
   render() {
     const { onItemAdded } = this.props;
     return (
-      <div className="input-group item-add-form">
+      <form className="input-group item-add-form d-flex"
+            onSubmit={this.onSubmit}
+      >
         <input
           className="form-control"
           type="text"
           placeholder="add task"
-          defaultValue="new task"
+          onChange={this.onLabelChange}
         />
         <button
           className="btn btn-outline-secondary"
-          onClick={() => onItemAdded('hi')}
         >Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
