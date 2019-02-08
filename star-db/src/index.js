34d9ebf64525f972b8793015f76c7254ce4d4249
endorphin82 <endorphin82@gmail.com>
2019-02-08 08:55:19 +0200

    0053-error_handling_fetch_api

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,13 +1,18 @@
-const url = 'https://swapi.co/api/people/1/';
+const url = 'https://swapi.co/api/people/14444/';
 
 const getResource = async (url) => {
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Cloud not fetch ${url},`
+      + ` recieved ${res.status}`)
+  }
   const body = await res.json();
   return body;
 };
 
 getResource(url)
-  .then((body) => console.log(body));
+  .then((body) => console.log(body))
+  .catch(err => console.log('Could not fetch', err))
 
 // fetch(url)
 //   .then(res => res.json())
