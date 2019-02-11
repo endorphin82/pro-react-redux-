e394588655e1d17215bfd8c8659946c6aa48e084
endorphin82 <endorphin82@gmail.com>
2019-02-11 18:13:58 +0200

    0069-componentdidcatch

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -4,6 +4,8 @@ import Header from "../header";
 import RandomPlanet from "../random-planet";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
 
 import "./app.css";
 
@@ -11,8 +13,14 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: null
+    selectedPerson: null,
+    hasError: false
   };
+
+  componentDidCatch() {
+    console.log("componentDidCatch()");
+    this.setState({ hasError: true });
+  }
 
   onPersonSelected = (id) => {
     this.setState({
@@ -29,7 +37,9 @@ export default class App extends Component {
   };
 
   render() {
-
+    if (this.state.hasError) {
+      return <ErrorIndicator/>;
+    }
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
@@ -39,11 +49,14 @@ export default class App extends Component {
         <Header/>
         {planet}
 
-        <button
-          className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
-          Toggle Random Planet
-        </button>
+        <div className="row mb2 button-row">
+          <button
+            className="toggle-planet btn btn-warning btn-lg"
+            onClick={this.toggleRandomPlanet}>
+            Toggle Random Planet
+          </button>
+          <ErrorButton/>
+        </div>
 
         <div className="row mb2">
           <div className="col-md-6">
