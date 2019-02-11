98432e49ccbf79e7aa90ab2497679e13334b1a59
endorphin82 <endorphin82@gmail.com>
2019-02-11 13:15:18 +0200

    0067-componentdidupdate_practice_spinner

--- star-db/src/components/person-details/person-details.js
+++ star-db/src/components/person-details/person-details.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import "./person-details.css";
 
 import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 export default class PersonDetails extends Component {
   state = {
-    person: null
+    person: null,
+    error: false,
+    loading: true
   };
 
   swapiService = new SwapiService();
@@ -14,12 +18,29 @@ export default class PersonDetails extends Component {
   componentDidMount() {
     this.updatePerson();
   }
+
   componentDidUpdate(prevProps) {
     // !!!
     if (this.props.personId !== prevProps.personId) {
       this.updatePerson();
+      this.setState({
+        loading: true
+      })
     }
   }
+
+  onPersonLoaded = () => {
+    this.setState({
+      loading: false
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    });
+  };
 
   updatePerson() {
     const { personId } = this.props;
@@ -31,42 +52,61 @@ export default class PersonDetails extends Component {
       .getPerson(personId)
       .then((person) => {
         this.setState({ person });
-      });
+      })
+      .then(this.onPersonLoaded)
+      .catch(this.onError);
+
   }
 
   render() {
     if (!this.state.person) {
       return <span>Select a person from a list</span>;
     }
+    const { loading, error } = this.state;
 
-    const {
-      id, name, gender,
-      birthYear, eyeColor
-    } = this.state.person;
+    const hasData = !(loading || error);
+    const errorMessage = error ? <ErrorIndicator/> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = hasData ? <PersonView person={this.state.person}/> : null;
 
     return (
       <div className="person-details card">
-        <img className="person-image"
-             src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}/>
-
-        <div className="card-body">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
-          </ul>
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     );
   }
 }
+
+const PersonView = ({person}) => {
+  const {
+    id, name, gender,
+    birthYear, eyeColor
+  } = person;
+
+  return (
+    <Fragment>
+      <img className="person-image"
+           src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}/>
+
+      <div className="card-body">
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Birth Year</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye Color</span>
+            <span>{eyeColor}</span>
+          </li>
+        </ul>
+      </div>
+    </Fragment>
+  );
+};
