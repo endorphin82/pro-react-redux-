feccb5293e5574f19574bc90ea04faaae92f9582
endorphin82 <endorphin82@gmail.com>
2019-02-12 13:11:24 +0200

    0071-0072-patterns_react_use_functions

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -5,11 +5,14 @@ import RandomPlanet from "../random-planet";
 import ErrorButton from "../error-button";
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
 
 import "./app.css";
+import SwapiService from "../../services/swapi-service";
 
 export default class App extends Component {
-
+  swapiService = new SwapiService();
   state = {
     showRandomPlanet: true,
     hasError: false
@@ -52,8 +55,31 @@ export default class App extends Component {
         </div>
 
         <PeoplePage/>
-        <PeoplePage/>
-        <PeoplePage/>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
+        </div>
+
       </div>
     );
   }
