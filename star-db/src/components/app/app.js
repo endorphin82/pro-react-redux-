a330aefbbc51c70d1e0dc15db97836b8af931fc1
endorphin82 <endorphin82@gmail.com>
2019-02-15 16:55:06 +0200

    0080-refactoring

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 import Header from "../header";
 import RandomPlanet from "../random-planet";
-import ItemDetails, { Record } from "../item-details";
 import ErrorIndicator from "../error-indicator";
-import ItemList from "../item-list";
 import Row from "../row";
 
 import "./app.css";
 import SwapiService from "../../services/swapi-service";
 import ErrorBoundry from "../error-boundry";
+import {
+  PersonList,
+  PlanetList,
+  StarshipList,
+  PersonDetails,
+  PlanetDetails,
+  StarshipDetails
+} from "../sw-components";
 
 export default class App extends Component {
   swapiService = new SwapiService();
@@ -39,84 +45,42 @@ export default class App extends Component {
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
-    const { getAllPeople, getAllPlanets, getPerson, getStarship, getPersonImage, getStarshipImage } = this.swapiService;
-    const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage}>
-        <Record field="gender" label="Gender"/>
-        <Record field="eyeColor" label="Eye Color"/>
-      </ItemDetails>
-    );
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-        <Record field="model" label="Model"/>
-        <Record field="length" label="Length"/>
-        <Record field="costInCredits" label="Cost"/>
-      </ItemDetails>
-    );
+    const {
+      getAllPeople, getAllPlanets,
+      getPerson, getStarship,
+      getPersonImage, getStarshipImage
+    } = this.swapiService;
+
     return (
       <ErrorBoundry>
         <div className="stardb-app container">
           <Header/>
-          <Row left={personDetails}
-               right={starshipDetails}
+          <Row
+            left={
+              <PersonList>
+                {({ name }) => <span>{name}</span>}
+              </PersonList>
+            }
+            right={<PersonDetails itemId={11} />}
           />
 
-          {/*{planet}*/}
+          <Row
+            left={
+              <PlanetList>
+                {({ name }) => <span>{name}</span>}
+              </PlanetList>
+            }
+            right={<PlanetDetails itemId={5} />}
+          />
+          <Row
+            left={
+              <StarshipList>
+                {({ name }) => <span>{name}</span>}
+              </StarshipList>
+            }
+            right={<StarshipDetails itemId={9} />}
+          />
 
-          {/*<div className="row mb2 button-row">*/}
-          {/*<button*/}
-          {/*className="toggle-planet btn btn-warning btn-lg"*/}
-          {/*onClick={this.toggleRandomPlanet}>*/}
-          {/*Toggle Random Planet*/}
-          {/*</button>*/}
-          {/*<ErrorButton/>*/}
-          {/*</div>*/}
-
-          {/*<PeoplePage/>*/}
-
-          {/*<div className="row mb2">*/}
-          {/*<div className="col-md-6">*/}
-          <ItemList
-            getData={getAllPeople}
-            onItemSelected={() => {}}
-          >
-
-              {({ name }) => <span>{name}</span>}
-          </ItemList>
-          {/*<br/>*/}
-          {/*<ItemList*/}
-            {/*getData={getAllPlanets}*/}
-            {/*onItemSelected={()=>{}}*/}
-          {/*>*/}
-
-              {/*{({ name }) => <span>{name}</span>}*/}
-          {/*</ItemList>*/}
-
-          {/*</div>*/}
-          {/*<div className="col-md-6">*/}
-          {/*<PersonDetails personId={this.state.selectedPerson}/>*/}
-          {/*</div>*/}
-          {/*</div>*/}
-
-          {/*<div className="row mb2">*/}
-          {/*<div className="col-md-6">*/}
-          {/*<ItemList*/}
-          {/*onItemSelected={this.onPersonSelected}*/}
-          {/*getData={this.swapiService.getAllStarships}*/}
-          {/*renderItem={({name, model}) => `${name} (${model})`}*/}
-
-          {/*/>*/}
-          {/*</div>*/}
-          {/*<div className="col-md-6">*/}
-          {/*<PersonDetails personId={this.state.selectedPerson}/>*/}
-          {/*</div>*/}
-          {/*</div>*/}
         </div>
       </ErrorBoundry>
     );
