11f56896c3878adefa30b3e46995d848cf992988
endorphin82 <endorphin82@gmail.com>
2019-02-22 05:37:28 +0200

    0111-action_creator

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,12 +1,12 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
 const reducer = (state = 0, action) => {
   switch (action.type) {
-    case 'RND':
+    case "RND":
       return state + action.payload;
-    case 'INC':
+    case "INC":
       return state + 1;
-    case 'DEC':
+    case "DEC":
       return state - 1;
     default:
       return state;
@@ -14,31 +14,33 @@ const reducer = (state = 0, action) => {
 };
 
 const store = createStore(reducer);
+
+const inc = () => ({ type: "INC" });
+const dec = () => ({ type: "DEC" });
+const rnd = (payload) => ({ type: "RND", payload });
+
 document
-  .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch({type: 'INC'})
+  .getElementById("inc")
+  .addEventListener("click", () => {
+    store.dispatch(inc());
   });
 
 document
-  .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch({type: 'DEC'})
+  .getElementById("dec")
+  .addEventListener("click", () => {
+    store.dispatch(dec());
   });
 
 document
-  .getElementById('rnd')
-  .addEventListener('click', () => {
-    const payload = Math.floor(Math.random()*10);
-    store.dispatch({
-      type: 'RND',
-      payload
-    })
+  .getElementById("rnd")
+  .addEventListener("click", () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(payload));
   });
 
 const update = () => {
   document
-    .getElementById('counter')
+    .getElementById("counter")
     .innerHTML = store.getState();
 };
 
