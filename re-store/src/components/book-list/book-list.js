8af0685692cc727db1590645cfa622e0b2c65a8d
endorphin82 <endorphin82@gmail.com>
2019-02-25 01:11:32 +0200

    0131-ownprops_mapdispatchtoprops

--- re-store/src/components/book-list/book-list.js
+++ re-store/src/components/book-list/book-list.js
@@ -13,15 +13,16 @@ import ErrorIndicator from "../error-indicator";
 class BookList extends Component {
 
   componentDidMount() {
-    const {
-      bookstoreService, booksLoaded,
-      booksRequested, booksError
-    } = this.props;
-
-    booksRequested();
-    bookstoreService.getBooks()
-      .then((data) => booksLoaded(data))
-      .catch((err) => booksError(err));
+    this.props.fetchBooks();
+    // const {
+    //   bookstoreService, booksLoaded,
+    //   booksRequested, booksError
+    // } = this.props;
+    //
+    // booksRequested();
+    // bookstoreService.getBooks()
+    //   .then((data) => booksLoaded(data))
+    //   .catch((err) => booksError(err));
   }
 
   render() {
@@ -47,10 +48,16 @@ class BookList extends Component {
 
 const mapStateToProps = ({ books, loading, error }) => ({ books, loading, error });
 
-const mapDispatchToProps = {
-  booksLoaded,
-  booksRequested,
-  booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  return {
+    fetchBooks: () => {
+      dispatch(booksRequested());
+      bookstoreService.getBooks()
+        .then((data) => dispatch(booksLoaded(data)))
+        .catch((err) => dispatch(booksError(err)));
+    }
+  };
 };
 
 export default compose(
